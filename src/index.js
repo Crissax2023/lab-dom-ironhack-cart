b8800ec060afda4e78d7812718531836c325e873
Cristian <Crissax2020@gmail.com>
2023-05-05 22:20:51 -0600

    Solved lab CR

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
+  const price = product.querySelector('.price span')
+  const quantity = product.querySelector('.quantity input')
   //... your code goes here
+  
+  const producto = price.innerHTML * quantity.value
+  const subtotal = product.querySelector('.subtotal span')
+
+  subtotal.innerHTML = producto  
+
+  return producto
 }
 
 function calculateAll() {
@@ -15,9 +23,19 @@ function calculateAll() {
 
   // ITERATION 2
   //... your code goes here
+  let productos = document.querySelectorAll('.product')
+  let totval = 0
 
+  for(let i = 0; i < productos.length; i++)
+  {
+    totval += updateSubtotal(productos[i])
+  }
   // ITERATION 3
   //... your code goes here
+  
+  let total = document.querySelector("#total-value span");
+  total.innerHTML = totval;
+  
 }
 
 // ITERATION 4
@@ -25,6 +43,9 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+  const tab = target.parentNode.parentNode
+  tab.parentNode.removeChild(tab)
+  
   //... your code goes here
 }
 
@@ -38,5 +59,12 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+
+    const remov = document.querySelector('btn-remove');
+    for(let i = 0; i < remov.length; i++) {
+      remov[i].addEventListener('click',function(e){
+        removeProduct(e)
+      })
+    }
   //... your code goes here
 });
